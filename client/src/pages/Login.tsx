898290a55a232ec4506c3b6fa7d4f8704ea5b65b
Yogeshachare <yogeshachare3@gmail.com>
2026-02-05 00:14:18 +0530

    10 th change

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="md:w-[60%] md:h-[70%] bg-white w-full h-full rounded-3xl flex">
+        <div className="h-full flex-1 flex justify-center items-center rounded-3xl bg-[url('./assets/ecomm.jpg')] bg-cover bg-center">
+          <h1 className="text-5xl font-bold text-[#23395b]">Order System</h1>
+        </div>
+        <div className="flex-1 flex flex-col justify-center items-center">
+          <input
+            type="text"
+            placeholder="Email"
+            className="h-10 p-2 border rounded-md mb-4"
+          />
+          <input type="password" placeholder="Password" />
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
